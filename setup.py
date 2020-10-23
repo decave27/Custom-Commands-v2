d6161b4c3fb1d9ec83c27db50b83332e0dcab685
Decave <decave27@gmail.com>
2020-10-24 07:50:11 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="Customcmd",
+    version="0.2",
+    license='MIT',
+    author="decave27",
+    author_email="decave27@gmail.com",
+    description="Custom-Command",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
